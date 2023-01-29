544260224dd8db0f43e53d443886a5454a940730
Chris Hargrove <hargrove.chris91@gmail.com>
2023-01-29 10:52:25 +0000

    feat: created the first script for menu setup

---
+++ Editor/Scripts/ToolsomeMenu.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Toolsome.Editor
+{
+    /// <summary>
+    /// Class that handles all of the Toolsome Menu setup.
+    /// </summary>
+    internal static class ToolsomeMenu
+    {
+        [MenuItem("Tools/Toolsome/Manager")]
+        public static void OpenPackageManager()
+        {
+        }
+
+        [MenuItem("Tools/Toolsome/Settings")]
+        public static void OpenSettings()
+        {
+        }
+    }
+}
